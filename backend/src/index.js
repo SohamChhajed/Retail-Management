c830db31879cb3c9d2ca5e7de39fb51ac54a42ed
SohamChhajed <sohamchhajed007@gmail.com>
2025-12-09 15:34:51 +0530

    backend uses process.env.port

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const transactionRoutes = require("./routes/transaction.routes");
-
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("api is running");
 });
 
+
 app.use("/api/retail/transactions", transactionRoutes);
+
 app.listen(PORT, () => {
-  console.log(`backend on http://localhost:${PORT}`);
+  console.log(`backend running on port ${PORT}`);
 });
